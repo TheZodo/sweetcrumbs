3df5412dc30c26793cde0a8098cb0e5100acc602
Zodo <malaizyogmuzumala@gmail.com>
2023-03-14 10:18:15 +0200

    more work

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import logo from '../assets/logo-final.jpg'
 export default function Home() {
   return (
-    <main className="bg-[#FAFAFA] min-h-screen p-6">
+    <main className="bg-[url('../assets/background-purple-cake-image.jpg')] min-h-screen p-6">
       <nav className="flex content-between justify-between">
         {/* <h1 className="font-extrabold text-4xl text-transparent bg-clip-text bg-gradient-to-r from-[#EFB6AE] to-[#4A1823] w-fit">
           SWEET
