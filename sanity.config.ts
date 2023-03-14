1f8066b7739003104bb3a8623708754aa3ef87e4
Zodo <malaizyogmuzumala@gmail.com>
2023-03-14 19:40:24 +0200

    more

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,16 +3,16 @@ import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
 export default defineConfig({
   basePath: '/studio',
-  name: 'SweetCrumbs_Admin',
-  title: 'SweetCrumbs',
+  name: 'Sweet_Crumbs_Admin',
+  title: 'Sweet Crumbs',
 
-  projectId,
-  dataset,
+  projectId: 'tyerv91u',
+  dataset: 'production',
 
   plugins: [deskTool(), visionTool()],
 
