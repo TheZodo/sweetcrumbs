6d7874e94e40e6abe563018d5f6f6b4ccf269a68
Zodo <malaizyogmuzumala@gmail.com>
2023-03-14 09:41:24 +0200

    more changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+import Image from 'next/image'
+import logo from '../assets/logo.jpg'
 export default function Home() {
   return (
     <main className="bg-[#FAFAFA] min-h-screen p-6">
       <nav className="flex content-between justify-between">
-        <h1 className="font-extrabold text-4xl text-transparent bg-clip-text bg-gradient-to-r from-[#EFB6AE] to-[#4A1823] w-fit">
+        {/* <h1 className="font-extrabold text-4xl text-transparent bg-clip-text bg-gradient-to-r from-[#EFB6AE] to-[#4A1823] w-fit">
           SWEET
-        </h1>
+        </h1> */}
+        <Image
+          src={logo}
+          alt={'SweetCrumbs Logo'}
+          width={80}
+          className=" rounded-full"
+        />
         <div></div>
       </nav>
       <div className="min-w-full flex items-center flex-col pt-20">
@@ -17,17 +25,18 @@ export default function Home() {
       </div>
       <div>
         <p className=" pt-16 px-3 font-bold">
-          Get your cake delivered to your doorstep within lusaka. We have a wide
-          range of cakes, from birthday cakes to wedding cakes. We also have a
-          wide range of flavors to choose from.
+          Get your <span className=" text-[#4A1823]"> CAKE</span> delivered to
+          your doorstep within lusaka. We have a wide range of cakes, from
+          birthday cakes to wedding cakes. We also have a wide range of flavors
+          to choose from.
         </p>
       </div>
       <section className="flex flex-col items-center pt-14">
-        <button className=" bg-[#4A1823] px-4 py-2 rounded-lg w-fit text-[#EFB6AE]">
+        <button className=" bg-[#4A1823] px-4 py-2 rounded-lg w-fit font-bold text-[#EFB6AE]">
           Build Your Cake
         </button>
         <span className=" flex flex-col items-center p-4">or</span>
-        <button className=" bg-[#4A1823] px-4 py-2 rounded-lg w-fit text-[#EFB6AE]">
+        <button className=" bg-[#4A1823] px-4 py-2 rounded-lg w-fit font-bold text-[#EFB6AE]">
           Choose a Popolar Cake
         </button>
       </section>
