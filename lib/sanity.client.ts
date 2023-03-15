3ed5108b10f3d68037a09dba704094ec6c7d1bf7
Zodo <malaizyogmuzumala@gmail.com>
2023-03-15 08:52:14 +0200

    more staging done

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,8 +1,11 @@
 import { createClient } from 'next-sanity'
 
+export const projectId: string = 'tyerv91u'
+export const dataset: string = 'production'
+
 export const client = createClient({
-  projectId: 'tyerv91u',
-  dataset: 'production',
+  projectId,
+  dataset,
   useCdn: true,
   apiVersion: '2023-03-14',
 })
