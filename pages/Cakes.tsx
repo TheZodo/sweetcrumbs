3cf2c031db83be5c8e7fd20a61f2797e0fe81e8a
Zodo <malaizyogmuzumala@gmail.com>
2023-03-15 06:49:50 +0200

    There's an issue

---
+++ pages/Cakes.tsx
@@ -0,0 +1,14 @@
+import CakeSegment from '@/components/CakeSegment'
+// import React from 'react'
+
+export default function Cakes() {
+  return (
+    <>
+      <div className="bg-[#EFB6AE] min-w-full ">
+        <CakeSegment />
+        <CakeSegment />
+        <CakeSegment />
+      </div>
+    </>
+  )
+}
