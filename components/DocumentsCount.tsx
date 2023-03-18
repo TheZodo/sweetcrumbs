3d6e1bff5218c7728bb8844f8324811b16236efc
Zodo <malaizyogmuzumala@gmail.com>
2023-03-18 15:05:07 +0200

    more work

--- components/DocumentsCount.tsx
+++ components/DocumentsCount.tsx
@@ -2,7 +2,7 @@ import groq from 'groq'
 
 export const query = groq`count(*[])`
 
-export function DocumentsCount({ data }) {
+export function DocumentsCount({ data }: any) {
   return (
     <>
       Documents: <strong>{data}</strong>
