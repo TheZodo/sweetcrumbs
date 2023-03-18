f9a9752ea47a2fadfb78ca4e8cb0c4fb93a55bb2
Zodo <malaizyogmuzumala@gmail.com>
2023-03-18 16:57:22 +0200

    Heller

---
+++ components/PreviewDocumentsCount.ts
@@ -0,0 +1,10 @@
+'use client'
+
+import { usePreview } from '../lib/sanity.preview'
+import { query } from './DocumentsCount'
+import DocumentsCount from './DocumentsCount'
+
+export default function PreviewDocumentsCount() {
+  const data = usePreview(null, query)
+  return <DocumentsCount data={data} />
+}
