f55f529eaa5303ebda3f61d6c0b8ceab431f3837
Zodo <malaizyogmuzumala@gmail.com>
2023-03-20 10:26:19 +0200

    more working on finishing this

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,22 @@ import Cakelist from '@/components/Cakelist'
 import { previewData } from 'next/headers'
 import Cakes from '@/pages/Cakes'
 import Hero from '@/pages/Hero'
+import { groq } from 'next-sanity'
+
+const query = groq`
+*[_type == "post"]{
+  ...,
+author->,
+categories[]->
+} | order(_createdAt desc)`
+
 export default function Home() {
+  if (previewData()) {
+    return <>previewData</>
+  }
+
   return (
     <main className="scrollbar-hide">
-      <Hero />
       {/* <Cakelist /> */}
       <Cakes />
     </main>
