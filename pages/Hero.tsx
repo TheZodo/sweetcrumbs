777cebf0373ca51777a1183a66ea393a7924e0e7
Zodo <malaizyogmuzumala@gmail.com>
2023-03-20 20:30:46 +0200

    more

--- pages/Hero.tsx
+++ pages/Hero.tsx
@@ -7,15 +7,15 @@ export default function Hero() {
     <div className="bg-[url('../assets/background-purple-cake-image.jpg')] min-h-screen p-6">
       <Navbar />
       <div className="min-w-full flex items-center flex-col pt-20">
-        <span className="font-extrabold text-5xl">For All Those</span> <br />
-        <span className="font-extrabold text-5xl text-transparent bg-clip-text bg-gradient-to-r from-[#EFB6AE] to-[#4A1823]">
+        <span className="font-bold text-5xl">For All Those</span> <br />
+        <span className="font-black text-5xl text-transparent bg-clip-text bg-gradient-to-r from-[#EFB6AE] to-[#4A1823]">
           SWEET
         </span>
         <br />
         <span className="font-extrabold text-5xl">Moments</span>
       </div>
       <div>
-        <p className=" pt-16 px-3 font-bold">
+        <p className=" pt-16 px-3 font-normal">
           Get your <span className=" text-[#4A1823]"> CAKE</span> delivered to
           your doorstep within lusaka. We have a wide range of cakes, from
           birthday cakes to wedding cakes. We also have a wide range of flavors
