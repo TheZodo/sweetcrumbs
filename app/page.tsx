f0a37980cb7fea5b4c91927b65a521f5a9789040
Zodo <malaizyogmuzumala@gmail.com>
2023-03-20 10:39:07 +0200

    more more

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { previewData } from 'next/headers'
 import Cakes from '@/pages/Cakes'
 import Hero from '@/pages/Hero'
 import { groq } from 'next-sanity'
+import { client } from '@/lib/sanity.client'
 
 const query = groq`
 *[_type == "post"]{
@@ -11,11 +12,12 @@ author->,
 categories[]->
 } | order(_createdAt desc)`
 
-export default function Home() {
+export default async function Home() {
   if (previewData()) {
     return <>previewData</>
   }
 
+  const posts = await client.fetch(query)
   return (
     <main className="scrollbar-hide">
       {/* <Cakelist /> */}
