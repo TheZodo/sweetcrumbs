273e3adbfaec13c58dd81eb4ed616405db504486
Zodo <malaizyogmuzumala@gmail.com>
2023-03-20 10:11:55 +0200

    preview working

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Cakelist from '@/components/Cakelist'
+import { previewData } from 'next/headers'
 import Cakes from '@/pages/Cakes'
 import Hero from '@/pages/Hero'
 export default function Home() {
   return (
     <main className="scrollbar-hide">
       <Hero />
-      <Cakelist />
+      {/* <Cakelist /> */}
       <Cakes />
     </main>
   )
