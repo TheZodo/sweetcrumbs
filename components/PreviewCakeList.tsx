6b1c8f74d3fdfd6d25aedf50e04f96c8a4461e80
Zodo <malaizyogmuzumala@gmail.com>
2023-03-20 11:44:44 +0200

    previewBloglist created

--- components/PreviewCakeList.tsx
+++ components/PreviewCakeList.tsx
@@ -1,3 +1,13 @@
 'use client'
 
 import { usePreview } from '@/lib/sanity.preview'
+import BlogList from './Bloglist'
+type Props = {
+  query: string
+}
+
+export default function PreviewCakeList({ query }: Props) {
+  const posts = usePreview(null, query)
+  console.log('loading posys...')
+  return <BlogList posts={posts} />
+}
