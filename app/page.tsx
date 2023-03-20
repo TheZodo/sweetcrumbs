7397a7c6ec736d9ef421daa12b3cd3c52a40bbf0
Zodo <malaizyogmuzumala@gmail.com>
2023-03-20 10:06:23 +0200

    more working

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Cakelist from '@/components/Cakelist'
 import Cakes from '@/pages/Cakes'
 import Hero from '@/pages/Hero'
 export default function Home() {
   return (
     <main className="scrollbar-hide">
       <Hero />
+      <Cakelist />
       <Cakes />
     </main>
   )
---
+++ components/Cakelist.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {
+  posts: Post[]
+}
+
+export default function Cakelist({ posts }: Props) {
+  return <div>Cakelist</div>
+}
